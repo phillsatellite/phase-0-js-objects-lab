7a0e165780a8dcfc64b596ccadd1a9452e1c1ebf
phillsatellite <pprints12@gmail.com>
2025-06-13 03:05:25 -0400

    Objects Lab Attempt 1

--- index.js
+++ index.js
@@ -1,6 +1,39 @@
-//Write your code here
+let attendee = {
+  attendeeId: "T001",
+  name: "Alice Smith",
+  event: "JavaScript Conference",
+  ticketType: "VIP",
+  ticketPrice: 150.00,
+}
+//Logs attendee name
+function logAttendeeName(attendee){
+  console.log(attendee.name);
+}
 
+//Logs ticket price
+function logTicketPrice(attendee){
+  console.log(attendee.ticketPrice);
+}
 
+//Updates ticket type 
+function updateTicketType(attendee, newTicketType){
+  attendee.ticketType = newTicketType;
+}
+
+//Updates ticket price 
+function updateTicketPrice(attendee, newTicketPrice){
+  attendee.ticketPrice = newTicketPrice;
+}
+
+//Removes the event property
+function removeEventProperty(attendee){
+  delete attendee.event;
+}
+
+//Adds a new property called checkedIn which should be true
+function addCheckedInProperty(attendee){
+  attendee.checkedIn = true;
+}
 
 //Needed for the tests to work. Don't modify
 module.exports = {
